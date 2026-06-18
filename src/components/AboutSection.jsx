60fb0eb3410bb78cbc002ec2c4d51cca17573eff
Praveen Thanvi <pthanvi10@gmail.com>
2026-06-18 23:11:22 +0530

    Update CV link path in AboutSection component

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./public/Praveen_Thanvi_CV.pdf"
+                href="../public/Praveen_Thanvi_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
